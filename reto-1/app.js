84dc1c513ac825d2a1efec67cfa2a505448f1a02
jairo <jairo_cz01@hotmail.com>
2020-05-30 10:22:51 -0500

    Se mejoró la entrada de texto en minuscula y mayuscula

--- reto-1/app.js
+++ reto-1/app.js
@@ -1,18 +1,29 @@
-let text = prompt("Ingrese texto a cifrar")
-let mayus = text.toUpperCase()
-let cifrado = ""
+let text = prompt("Ingrese texto a cifrar");
+// let mayus = text.toUpperCase()
+let cifrado = "";
 
 function cifrar(str) {
-    for (let i = 0; i < str.length; i++) {
-        // console.log(str[i].charCodeAt())
-        let numAscii = str[i].charCodeAt()
-        if(numAscii >= 65 && numAscii <= 77) {
-            cifrado += String.fromCharCode(numAscii + 13)
-        } else if (numAscii >= 78 && numAscii <= 90) {
-            cifrado += String.fromCharCode(numAscii - 13)
-        }
+  for (let i = 0; i < str.length; i++) {
+    let lower = str.toLowerCase();
+    // let mayus = str.toUpperCase()
+    // console.log(str[i].charCodeAt())
+    let numAscii = str[i].charCodeAt();
+
+    if (str === lower) {
+      if (numAscii >= 97 && numAscii <= 109) {
+        cifrado += String.fromCharCode(numAscii + 13);
+      } else if (numAscii >= 110 && numAscii <= 122) {
+        cifrado += String.fromCharCode(numAscii - 13);
+      }
+    } else {
+      if (numAscii >= 65 && numAscii <= 77) {
+        cifrado += String.fromCharCode(numAscii + 13);
+      } else if (numAscii >= 78 && numAscii <= 90) {
+        cifrado += String.fromCharCode(numAscii - 13);
+      }
     }
-    return cifrado
+  }
+  return cifrado;
 }
 
-console.log("La palabra cifrada es", cifrar(mayus))
+console.log("La palabra cifrada es", cifrar(text));
